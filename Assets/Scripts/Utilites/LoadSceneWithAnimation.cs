fc2b2c883abf8138920441e17e2b05d746469c5b
Aaron Andrews <WindowWasher@users.noreply.github.com>
2018-11-20 14:50:44 -0700

    Added start menu with simple scene transition.

---
+++ Assets/Scripts/Utilites/LoadSceneWithAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneWithAnimation : MonoBehaviour {
+
+    public Animator transitionAnimation;
+    public int sceneToLoadIndex = 1;
+
+    public void StartGame()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnimation.SetTrigger("StartGame");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(sceneToLoadIndex);
+    }
+	
+}
