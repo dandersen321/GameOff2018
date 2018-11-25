ea7871d61a876c0f563831a5e10c0144adb7593b
Aaron Andrews <WindowWasher@users.noreply.github.com>
2018-11-25 14:30:49 -0700

    Created a custom font for the game

---
+++ Assets/Scripts/UI/GameMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour {
+
+    public GameObject gameMenu;
+
+	// Use this for initialization
+	void Start () {
+        gameMenu.SetActive(false);
+	}
+
+    public void ToggleGameMenu()
+    {
+        gameMenu.SetActive(!gameMenu.activeSelf);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleGameMenu();
+        }
+	}
+}
--- Assets/Scripts/Utilites/ExitGame.cs
+++ Assets/Scripts/Utilites/ExitGame.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitGame : MonoBehaviour {
+
+    public int mainMenuScene = 0;
 
     public void ExitGameNow()
     {
         Application.Quit();
+    }
+
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
     }
 }
