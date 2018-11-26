f3e9b4849c12048476580924864db9527ff49ba2
Aaron Andrews <WindowWasher@users.noreply.github.com>
2018-11-25 17:16:19 -0700

    Added story scene transition

---
+++ Assets/Scripts/UI/StoryScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StoryScene : MonoBehaviour {
+
+    public Text storyText;
+    public Text buttonText;
+    public List<string> lines;
+    public int sceneToLoad = 2;
+
+    private int line = 0;
+
+    public void Nextline()
+    {
+        if (line < lines.Count)
+        {
+            storyText.text = lines[line];
+            line++;
+
+            if (line == lines.Count)
+                buttonText.text = "Start Game";
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneToLoad); 
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        Nextline();
+	}
+}
