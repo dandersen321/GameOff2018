553a943e9d220b41e16f3ebb8c60a484c1f6516a
Kyle Criddle <criddle.kyle@gmail.com>
2018-11-29 23:22:09 -0700

    - added the ending scene text to EndScene.cs - added some protection in getting dialog for that day/speaker. if there is no dialog for that day, or none for that speaker on that day, then a default dialog is displayed for that speaker. - made the lookup of day into json file based on the day key, not the index in the array. - added dialog to tutorial for explaining some game mechanics loosely. - added story/plot dialog for the post-tutorial days.

--- Assets/Scripts/Dialog/Json2Dialog.cs
+++ Assets/Scripts/Dialog/Json2Dialog.cs
@@ -18,7 +18,6 @@ public class Speakers {
 }
 
 public class SpeakerDialogObj {
-    public string name    { get; set; } // overrides speaker
     public string speaker { get; set; }
     public string text    { get; set; }
 }
@@ -76,20 +75,26 @@ public class Json2Dialog {
 
     public static List<DayDialog> getDialogListForDay(Speaker speaker, int day)
     {
-	int maxChars = 76;
-        var obj               = DialogJsonParser.getDialog();
-        var objDayDialog      = obj.DayDialog[day]; // current day counter should be zero based.
-
+        var obj             = DialogJsonParser.getDialog();
+        var objDayDialog    = getDialogOfThatDay(obj, day);
         DayDialog dayDialog = new DayDialog();
         dayDialog.day       = day;
 
+        // if no dialog defined for that day, return default
+        if (objDayDialog == null) {
+            return getDefaultDialog(obj, speaker, dayDialog);
+        }
+
+        // if no dialog defined for that speaker, return default
         var dialogList = getDialogList(objDayDialog, speaker);
         if (dialogList == null) {
-            throw new System.Exception("dialog null! probably syntax error in dialog.json!");
+            return getDefaultDialog(obj, speaker, dayDialog);
         }
+
 	// split the text into an array of lines to be displayed on screen.
 	// if the next word is > than the size we allow in the text area,
 	// add it to the next line.
+	int maxChars = 76;
         foreach (var dialog in dialogList) {
             string text    = dialog.text;
 	    string[] words = text.Split(new char [] {' '});
@@ -111,6 +116,29 @@ public class Json2Dialog {
         var dayDialogList = new List<DayDialog>();
         dayDialogList.Add(dayDialog);
         return dayDialogList;
+    }
+
+    private static DayDialogObj getDialogOfThatDay(DialogJsonObj obj, int day)
+    {
+        // day counter should be zero based.
+        foreach (var _dayDialog in obj.DayDialog) {
+            if (_dayDialog.day == day) {
+                return _dayDialog;
+            }
+        }
+        return null;
+    }
+
+    private static List<DayDialog> getDefaultDialog(DialogJsonObj obj, Speaker speaker, DayDialog dayDialog)
+    {
+        var speakerObj        = getSpeaker(obj, speaker);
+        var defaultDialogList = new List<DayDialog>();
+        Line line             = new Line();
+        line.name             = speakerObj.name;
+        line.line             = speakerObj.defaultDialog;
+        dayDialog.lines.Add(line);
+        defaultDialogList.Add(dayDialog);
+        return defaultDialogList;
     }
 
     private static IList<SpeakerDialogObj> getDialogList(DayDialogObj obj, Speaker speaker)
