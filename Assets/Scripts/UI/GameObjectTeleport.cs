262696b120fb9aa6b491a52f864a99f0ddaa353c
Aaron Andrews <WindowWasher@users.noreply.github.com>
2018-11-30 18:55:55 -0700

    Added code to move the alien after the first night.

---
+++ Assets/Scripts/UI/GameObjectTeleport.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectTeleport : MonoBehaviour {
+
+    public GameObject objectToMove;
+    public GameObject locationToMoveTo;
+
+    public void OnNightBeat(int nightBeat)
+    {
+        if (nightBeat == 0)
+        {
+            objectToMove.transform.position = locationToMoveTo.transform.position;
+            objectToMove.transform.rotation = locationToMoveTo.transform.rotation;
+        }
+    }
+
+    private void OnEnable()
+    {
+        References.GetEnemySpawnerManager().OnNightBeat += OnNightBeat;
+
+    }
+
+    private void OnDisable()
+    {
+        References.GetEnemySpawnerManager().OnNightBeat -= OnNightBeat;
+    }
+
+}
