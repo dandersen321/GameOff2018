ad9f0ffa87dd4e6be2dc5f77f61dea9c18c616b7
Aaron Andrews <WindowWasher@users.noreply.github.com>
2018-12-01 03:43:45 -0700

    Adjusted waves 8 and 9

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -23,9 +23,9 @@ public class InventoryManager : MonoBehaviour {
     {
         foreach (ChickenType chickenType in chickenInventories)
         {
-            chickenType.chickenCount = 0;
-            chickenType.seedCount = 0;
-            chickenType.currentRank = 0;
+            chickenType.chickenCount = 50;
+            chickenType.seedCount = 30;
+            chickenType.currentRank = 3;
             //if (chickenType.name == ChickenTypeEnum.explosiveName)
             //{
             //   chickenType.
